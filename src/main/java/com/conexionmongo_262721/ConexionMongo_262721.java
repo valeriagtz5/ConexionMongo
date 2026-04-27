b32f85de68f95b571d6ca388149e902856f7fe40
Valeria Gtz <valeria.gutierrez262721@potros.itson.edu.mx>
2026-04-27 14:31:16 -0700

    Primera parte

---
+++ src/main/java/com/conexionmongo_262721/ConexionMongo_262721.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.conexionmongo_262721;
+
+/**
+ *
+ * @author valeria
+ */
+public class ConexionMongo_262721 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
